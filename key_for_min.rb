3ce686c16c17413c11d55da5188ce7ea89be6d22
Liz Love <love.elizabeth1989@gmail.com>
2020-06-18 19:38:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  num = 0
-  name_hash.each do |key, value|
-    if value <= num
-      value = key
+  name_hash.min do |key, value|
+    
     end
   end
 end
